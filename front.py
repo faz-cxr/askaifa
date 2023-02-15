083988893b1cc2cb3eb9eac3f3abd023f08d704c
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2023-02-15 07:15:33 +0530

    Update front.py

--- front.py
+++ front.py
@@ -1,39 +1,42 @@
 """Python file to serve as the frontend"""
-import streamlit as st
-from streamlit_chat import message
-import faiss
-from langchain import OpenAI
-from langchain.agents import Tool
-from langchain.chains.conversation.memory import ConversationBufferMemory
-from langchain import OpenAI
-from langchain.agents import initialize_agent
-from gpt_index import GPTSimpleVectorIndex
 import os
+import streamlit as st
 import openai
- 
+
+from gpt_index import GPTSimpleVectorIndex
+from pathlib import Path
+from gpt_index import download_loader
+from streamlit_chat import message
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Load the LangChain.
+history_input = []
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+def get_text():
+    input_text = st.text_input("You: ", key="input")
+    return input_text 
+
+user_input = get_text()
 index = GPTSimpleVectorIndex.load_from_disk('SVindex-BMJ.json')
 
-tools = [
-    Tool(
-        name = "GPT Index",
-        func=lambda q: str(index.query(q)),
-        description="useful for when you want to answer questions about the author. The input to this tool should be a complete english sentence.",
-        return_direct=True
-    ),
-]
+if user_input:
+    response = index.query(user_input, response_mode="tree_summarize")
+    output = str(response)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
 
-memory = ConversationBufferMemory(memory_key="chat_history")
-llm=OpenAI(temperature=0)
+if st.session_state['generated']:
 
-chain = initialize_agent(tools, llm, agent="conversational-react-description", memory=memory)
-
-
-# From here down is all the StreamLit UI.
-st.set_page_config(page_title="Aifa QA Bot", page_icon=":robot:")
-st.header("Aifa QA Bot")
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
